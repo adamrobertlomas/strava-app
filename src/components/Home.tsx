a5466a49ea7c95d0adadba881fca31d0553c06fd
Adam Lomas <adam.lomas@emishealth.com>
2019-08-02 14:20:33 +0100

    Move axios post to separate function

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import AthleteSummary from "./AthleteSummary";
 import Login from "./Login";
 import React from "react";
-import axios from "axios";
+import { PostResponse } from "../Api/PostResponse";
 
 const clientId: any = process.env.REACT_APP_CLIENT_ID;
 const clientSecret: any = process.env.REACT_APP_CLIENT_SECRET;
@@ -23,29 +23,26 @@ class Home extends React.Component<{}, IHomeState> {
     this.getToken();
   }
 
-  getToken = () => {
+  getToken = async () => {
+    const url = "/oauth/token";
     const urlParams = new URLSearchParams(window.location.search);
     const paramsCode = urlParams.get("code");
+    const config = {
+      client_id: clientId,
+      client_secret: clientSecret,
+      code: paramsCode,
+      grant_type: "authorization_code"
+    };
 
-    axios
-      .post("https://www.strava.com/oauth/token", {
-        client_id: clientId,
-        client_secret: clientSecret,
-        code: paramsCode,
-        grant_type: "authorization_code"
-      })
-      .then(response => {
-        console.log(`Axios: ${response.data.access_token}`);
-        return response.data.access_token;
-      })
-      .then(token => {
-        this.setState({
-          accessToken: token
-        });
-      })
-      .catch(error => {
-        console.log(error);
+    const response = await PostResponse(config, url);
+
+    if (response.data) {
+      this.setState({
+        accessToken: response.data.access_token
       });
+    } else {
+      console.log(response.error);
+    }
   };
 
   render() {
