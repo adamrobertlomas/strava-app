78f12929925f9e4edafa6a03b1549a705157ecef
Adam Lomas <adam.lomas@emishealth.com>
2020-04-07 10:16:11 +0100

    Fresh start and switch to yarn

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Main from "./Components/Main";
-
-const App: React.FC = () => {
-  return <Main />;
-};
-
-export default App;
--- src/types/IAthlete.tsx
+++
@@ -1,6 +0,0 @@
-export interface IAthlete {
-  id: number;
-  username: string;
-  firstname: string;
-  lastname: string;
-}
